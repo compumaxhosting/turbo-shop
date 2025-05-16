93b3866dc849a23b97b6800c9a3cfc10c5c7fcfd
ibrahimAbrar7 <mohdabrar41111@gmail.com>
2025-05-16 18:48:39 +0530

    Update saab turbo images

---
+++ src/app/saab-products/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import ProductsSection from "@/components/products-listing/ProductsSection";
+import React from "react";
+import { saabProductsData } from "@/data/products-data/saabProductsData";
+import HeaderTop from "@/components/header/HeaderTop";
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+import BreadcrumbSection from "@/components/products-listing/BreadcrumbSection";
+import { SelectVehicleMake } from "@/components/extras/SelectVehicleMake";
+import usePreLoaderAnimation from "@/lib/usePreLoaderAnimation";
+import ScrollToTop from "@/components/extras/ScrollToTop";
+import ShippingFeeToast from "@/components/extras/ShippingFeeToast";
+
+const Page = () => {
+  usePreLoaderAnimation();
+
+  return (
+    <>
+      <HeaderTop />
+      <Header />
+      <SelectVehicleMake />
+      <ShippingFeeToast
+        heading="SHIPPING CHARGES FOR REPAIR"
+        text="ADDITIONAL $200 FOR SHIPPING FOR PRODUCT REPAIRS."
+      />
+      <BreadcrumbSection
+        title="SHOP SAAB PRODUCTS"
+        breadcrumbs={[{ label: "Home", href: "/" }, { label: "saab-products" }]}
+      />
+      <ProductsSection productsData={saabProductsData} />
+      <ScrollToTop />
+      <Footer />
+    </>
+  );
+};
+
+export default Page;
