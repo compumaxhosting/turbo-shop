5a2639fe183cd5c21632e6db13dbea2f19b964c9
compumaxhosting <compumaxhost@gmail.com>
2025-07-11 18:41:07 +0530

    updted few things

---
+++ src/app/dodge-chrysler-eagle-jeep/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import ProductsSection from "@/components/products-listing/ProductsSection";
+import React from "react";
+import { dodgeProductsData } from "@/data/products-data/dodgeProductsData";
+import HeaderTop from "@/components/header/HeaderTop";
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+import BreadcrumbSection from "@/components/products-listing/BreadcrumbSection";
+import { SelectVehicleMake } from "@/components/extras/SelectVehicleMake";
+import usePreLoaderAnimation from "@/lib/usePreLoaderAnimation";
+import ScrollToTop from "@/components/extras/ScrollToTop";
+import ShippingFeeToast from "@/components/extras/ShippingFeeToast";
+
+const Page = () => {
+  usePreLoaderAnimation();
+
+  return (
+    <>
+      <HeaderTop />
+      <Header />
+      <SelectVehicleMake />
+      <ShippingFeeToast
+        heading="SHIPPING CHARGES FOR REPAIR"
+        text="ADDITIONAL $200 FOR SHIPPING FOR PRODUCT REPAIRS."
+      />
+      <BreadcrumbSection
+        title="SHOP Dodge/Chrysler/Eagle/Jeep PRODUCTS"
+        breadcrumbs={[
+          { label: "Home", href: "/" },
+          { label: "dodge-products" },
+        ]}
+      />
+      <ProductsSection productsData={dodgeProductsData} />
+      <ScrollToTop />
+      <Footer />
+    </>
+  );
+};
+
+export default Page;
---
+++ src/data/products-data/nissanInfiniti.ts
@@ -0,0 +1,68 @@
+import { commonDescription } from "../commonDescription";
+
+export const nissanInfinitiProductsData = [
+  {
+    id: 1,
+    imgPath: [
+      "/hero-img.png",
+      "/hero-img.png",
+      "/hero-img.png",
+      "/hero-img.png",
+    ],
+    brand: "Nissan",
+    modelName: "Juke 2011-2017 MR16DDT",
+    subTitle: "",
+    leftPartNumber: "144111KC0E",
+    rightPartNumber: "",
+    prices: {
+      usd: {
+        leftCurrent: "$450.00 USD",
+        leftOriginal: "$550.00 USD",
+        rightCurrent: "",
+        rightOriginal: "",
+      },
+      cad: {
+        leftCurrent: "$650.00 CAD",
+        leftOriginal: "$750.00 CAD",
+        rightCurrent: "",
+        rightOriginal: "",
+      },
+    },
+    description: commonDescription,
+    category: "Nissan",
+    year: "",
+    tag: "popular",
+  },
+  {
+    id: 2,
+    imgPath: [
+      "/hero-img.png",
+      "/hero-img.png",
+      "/hero-img.png",
+      "/hero-img.png",
+    ],
+    brand: "Nissan",
+    modelName: "Cooper B48 Engines",
+    subTitle: "",
+    leftPartNumber: "11658626637",
+    rightPartNumber: "",
+    prices: {
+      usd: {
+        leftCurrent: "$450.00 USD",
+        leftOriginal: "$550.00 USD",
+        rightCurrent: "",
+        rightOriginal: "",
+      },
+      cad: {
+        leftCurrent: "$650.00 CAD",
+        leftOriginal: "$750.00 CAD",
+        rightCurrent: "",
+        rightOriginal: "",
+      },
+    },
+    description: commonDescription,
+    category: "Nissan",
+    year: "",
+    tag: "popular",
+  },
+];
